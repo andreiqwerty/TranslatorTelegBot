fef9ef828ce76057391cfb75b88ecbb1b2270d9e
Andrei <andrei.tuchin.job@gmail.com>
2020-08-06 21:34:33 +0300

    change lang

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
 import domain.model.TranslatorBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
     public static void main(String[] args) {
         try {
+            logger.info("Start bot");
             ApiContextInitializer.init();
             TelegramBotsApi botsApi = new TelegramBotsApi();
             botsApi.registerBot(TranslatorBot.getInstance());
         } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
+            logger.error("Telegram API doesn't work", e);
         }
     }
 }
